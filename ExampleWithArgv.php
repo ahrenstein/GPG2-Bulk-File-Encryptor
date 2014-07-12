bad7971b7e743959c33d26d81e8d83d192656046
Matthew Ahrenstein <matt@ahrenstein.com>
2014-07-12 14:57:42 -0400

    CLI parameters example missing recipient argument
    I forgot to add the argument for CLI to choose the encryption
    recipient. This has been added but it’s syntax is not checked.

--- ExampleWithArgv.php
+++ ExampleWithArgv.php
@@ -11,17 +11,19 @@
 require_once("includes/GPGBulkFileEncryptor.class.php"); //Require the GPG Bulk File Encryptor class
 
 $directory       = $argv[1]; //Store our directory in a variable
-$deleteOriginals = ($argv[2] === 'true'); //if the string is true then it is boolean true and files will be deleted. Anything else matches false to be safe
+$recipient = $argv[2];
+$deleteOriginals = ($argv[3] === 'true'); //if the string is true then it is boolean true and files will be deleted. Anything else matches false to be safe
 
 //The below if/else is used to check if CLI arguments were properly filled out.
-if (count($argv) == 3 AND file_exists($argv[1]) == TRUE) //Check to make sure we have 2 arguments, and a valid directory path (the script counts as an entry in the argv array)
+if (count($argv) == 4 AND file_exists($argv[1]) == TRUE) //Check to make sure we have 3 arguments, and a valid directory path (the script counts as an entry in the argv array)
 {
 	//Do nothing
 }
 else
 {
-	echo "Usage: " . $argv[0] . " <directory to recursively encrypt> <delete files>\n";
+	echo "Usage: " . $argv[0] . " <directory to recursively encrypt> <recipient key e-mail. <delete files>\n";
 	echo "directory to recursively encrypt: Must be a valid existing directory\n";
+	echo "recipient key e-mail: Must be a valid e-mail address of someone in your GPG keyring\n";
 	echo "delete files: true/false\n";
 	exit;
 }
